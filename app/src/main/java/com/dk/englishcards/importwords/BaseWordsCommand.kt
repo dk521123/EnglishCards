308d6521b77ddfa0a31e97831b679ef85cb5ef3f
DK <dk521123@yahoo.co.jp>
2020-08-30 12:41:32 +0900

    To add English words and modify import function

--- app/src/main/java/com/dk/englishcards/importwords/BaseWordsCommand.kt
+++ app/src/main/java/com/dk/englishcards/importwords/BaseWordsCommand.kt
@@ -22,7 +22,12 @@ abstract class BaseWordsCommand(assertManager: AssetManager? = null, csvFilePath
             val targetEnglishCards = mutableListOf<EnglishCard>()
             val csvContents = csvReader.readCsv(
                 1, "UTF-8")
-            for (csvItems in csvContents) {
+            for ((index, csvItems) in csvContents.withIndex()) {
+                Log.i("execute", "Starting [$index]")
+                if (csvItems.size < 2) {
+                    Log.w("execute", "Invalid data size ${csvItems.size} [$index]")
+                    continue
+                }
                 val english = csvItems[0]
                 val motherLanguage = csvItems[1]
                 val memo = if (csvItems.size >= 3) csvItems[2] else ""
